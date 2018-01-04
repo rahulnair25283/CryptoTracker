56def84d09266fa81bdc0b57931caa38feeefdf7
Rahul Nair <rahul.nair@aginicx.com>
2018-01-04 20:58:28 +1000

    api call with fetch and initial display

---
+++ src/model.ts
@@ -0,0 +1,17 @@
+export interface Crypto {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: string;
+  priceUSD: string;
+  priceBTC: string;
+  twentyFourHourVolumeUSD: string;
+  marketCapUSD: string;
+  availableSupply: string;
+  totalSupply: string;
+  maxSupply: String;
+  percentChangeIn1Hour: string;
+  percentChangeIn24Hrs: string;
+  percentChangeIn7Days: string;
+  lastUpdated: string;
+}
