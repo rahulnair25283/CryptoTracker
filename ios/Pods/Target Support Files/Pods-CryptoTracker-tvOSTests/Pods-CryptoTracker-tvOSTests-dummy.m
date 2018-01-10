d294852e8de986221d4089235d9a951eb116dae3
Rahul Nair <rahul.nair@aginicx.com>
2018-01-10 21:10:45 +1000

    refresh control + vs app-center sdk integration

---
+++ ios/Pods/Target Support Files/Pods-CryptoTracker-tvOSTests/Pods-CryptoTracker-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CryptoTracker_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_CryptoTracker_tvOSTests
+@end
--- src/components/CryptoList.tsx
+++ src/components/CryptoList.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import TimerMixin from "react-timer-mixin";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, View, RefreshControl } from "react-native";
 import { Crypto } from "../model";
 import CryptoItem from "./CryptoItem";
 import styles from "./styles";
@@ -24,15 +24,7 @@ export default class CryptoList extends Component<Props, State> {
 
   public componentDidMount() {
     this.timer = TimerMixin.setInterval(() => {
-      return fetch("https://api.coinmarketcap.com/v1/ticker/")
-        .then(response => response.json())
-        .then(responseJson =>
-          this.setState({
-            loading: false,
-            data: responseJson,
-          })
-        )
-        .catch(err => console.log(err));
+      return this.refreshContent();
     }, 1000);
   }
 
@@ -50,6 +42,13 @@ export default class CryptoList extends Component<Props, State> {
           renderItem={this.renderItem}
           ItemSeparatorComponent={this.renderSeparator}
           stickyHeaderIndices={[0]}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.loading}
+              onRefresh={this.onRefresh}
+              tintColor="#fc5130"
+            />
+          }
         />
       </View>
     );
@@ -62,11 +61,34 @@ export default class CryptoList extends Component<Props, State> {
   private renderHeader = () => (
     <View style={styles.header}>
       <Text style={styles.headerText}>Cryptocurrencies</Text>
-      <View style={{left: 95, justifyContent: "center", alignItems: "center"}}>
-         <Icon name="search" color="#ffffff" size={20} />
+      <View
+        style={{ left: 95, justifyContent: "center", alignItems: "center" }}
+      >
+        <Icon name="search" color="#ffffff" size={20} />
       </View>
     </View>
-  );
+  )
 
   private renderSeparator = () => <View style={styles.separator} />;
+
+  private onRefresh = () => {
+    this.setState({
+      loading: true,
+    });
+    this.refreshContent();
+    this.setState({
+      loading: false,
+    });
+  }
+
+  private refreshContent = () =>
+    fetch("https://api.coinmarketcap.com/v1/ticker/")
+      .then(response => response.json())
+      .then(responseJson =>
+        this.setState({
+          loading: false,
+          data: responseJson,
+        })
+      )
+      .catch(err => console.log(err))
 }
