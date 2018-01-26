037da38de785a6f19058633b0acc3cfe9ffb10b4
Rahul Nair <rahul.nair@aginicx.com>
2018-01-26 22:19:56 +1000

    hackery to get the app building on VS App Center

---
+++ src/index.ios.ts
@@ -0,0 +1,5 @@
+import { AppRegistry } from "react-native";
+import Root from "./Root";
+
+console.ignoredYellowBox = ["Remote debugger"];
+AppRegistry.registerComponent("CryptoTracker", () => Root);
